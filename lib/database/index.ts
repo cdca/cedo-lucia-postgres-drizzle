fcf7df2ad75d445c7fe8f63def0d86ea332f43be
Cedomir Sobic <cedomir.sobic@gmail.com>
2024-03-13 22:38:45 +0100

    Remove additional DB checks

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -3,15 +3,6 @@ import { NodePgDatabase, drizzle } from "drizzle-orm/node-postgres";
 
 import * as schema from "./schema";
 
-if (
-  !process.env.DB_HOST ||
-  !process.env.DB_NAME ||
-  !process.env.DB_USER ||
-  !process.env.DB_PASSWORD
-) {
-  throw new Error("Database credentials missing.");
-}
-
 const pool = new Pool({
   connectionString: process.env.DB_URL,
 });
